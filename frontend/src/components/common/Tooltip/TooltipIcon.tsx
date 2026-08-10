0f267ee3e6eecec8e41b4064233c387c50f76e39
Rucha0901 <imt_2025071@iiitm.ac.in>
2026-08-10 22:05:15 +0530

    fix: address tooltip children prop warning and update snapshots

--- frontend/src/components/common/Tooltip/TooltipIcon.tsx
+++ frontend/src/components/common/Tooltip/TooltipIcon.tsx
@@ -26,9 +26,10 @@ export interface TooltipIconProps {
 }
 
 const IconReffed = React.forwardRef((props: IconProps, ref: any) => {
+  const { icon, ...otherProps } = props;
   return (
-    <Container ref={ref} sx={{ display: 'inline', padding: '0 .3rem' }}>
-      <Icon {...props} />
+    <Container ref={ref} sx={{ display: 'inline', padding: '0 .3rem' }} {...otherProps}>
+      <Icon icon={icon!} />
     </Container>
   );
 });
